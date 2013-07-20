1631021cfb03f9301213ca2f8e4eabafdfa56b07
irb <engmohamans@yahoo.com>
2013-07-20 11:40:34 +0200

    Car rental module based on product module where you can specify the vehicle information and also you can establish a rental contract

---
+++ __openerp__.py
@@ -0,0 +1,17 @@
+{
+  
+   'name' : 'Car Rental Module',
+   'authod' : 'Mohammed Mansour',
+   'website':'https://www.github.com//mohammedmansour',
+   'version' : '1.0',
+   'description' : '''This is a module for automating the car rental process''',
+   'depends' : ['base' , 'product'],
+   'category':'General',
+   'init_xml':[],
+   'data':['car_rental_view.xml'],
+   'demo_xml':[],
+   'active':False,
+   'installable':True,
+   'certificate':''
+    
+}
---
+++ car_rental.py
@@ -0,0 +1,185 @@
+import time
+from datetime import date
+from datetime import datetime
+from datetime import timedelta
+from dateutil import relativedelta
+import netsvc
+from osv import fields, osv
+import tools
+from tools.translate import _
+import decimal_precision as dp
+
+
+###################################
+#This Object to specify the veihcle make
+
+class car_make(osv.osv):
+    _name='car.make'
+    _description='this module to add a new veihcle make'
+    _columns={
+              'name':fields.char('Car Make',size=100,required=True,translate=True),
+              'car_rental_id': fields.one2many('car.rental', 'car_make_id' , 'Veihcle Make'),
+                             
+             }
+car_make()  
+
+###################################
+#This Object to specify the veihcle brand
+
+class car_brand(osv.osv):
+    _name='car.brand'
+    _description='this module to add a new veihcle brand'
+    _columns={
+              'name':fields.char('Car Brand',size=100,required=True,translate=True),
+              'car_rental_id': fields.one2many('car.rental', 'car_brand_id' , 'Veihcle Brand'),
+               
+             }
+car_brand()  
+
+    
+###################################
+#This Object to specify the veihcle class
+
+class car_class(osv.osv):
+    _name='car.class'
+    _description='this module to add a new veihcle class'
+    _columns={
+              'name':fields.char('Car Class',size=100,required=True,translate=True),
+              'car_rental_id': fields.one2many('car.rental', 'car_class_id' , 'Veihcle Class'),
+               
+             }
+car_class()  
+
+###################################
+#This Object to specify the veihcle color
+
+class car_color(osv.osv):
+    _name='car.color'
+    _description='this module to add a new veihcle color'
+    _columns={
+              'name':fields.char('Car Class',size=100,required=True,translate=True),
+              'car_rental_id': fields.one2many('car.rental', 'car_color_id' , 'Veihcle Color'),
+               
+             }
+car_color()  
+
+###################################
+# this Object to determine the veihcle information
+
+#class veihcle_info(osv.osv):
+class car_rental(osv.osv):
+
+    _name='car.rental'
+    _inherits = {'product.product': 'product_id'}
+    #_inherit='product.product'
+    _description='Module for Veihcle Information'
+    _columns={              
+              'car_make_id': fields.many2one('car.make', 'Vehicle Make'),
+              'car_brand_id': fields.many2one('car.brand', 'Vehicle Brand'),
+              'car_class_id': fields.many2one('car.class', 'Vehicle Class'),
+              'car_color_id': fields.many2one('car.color', 'Vehicle Color'),              
+              'notes':fields.text('Details'),
+              
+              'name':fields.char('Vehicle Name',size=20,required=True),
+              'regnno':fields.char('Vehicle Registration #',size=11,required=True),
+              'company':fields.many2one('res.company','Company',required=True),              
+              'year':fields.char('Year',size=4),              
+              'serial':fields.char('productSerial #',size=50),
+              'type': fields.selection([
+                         ('truck','Truck'),
+                         ('bus','Bus'),
+                         ('car','Car')], 'Class', required=True,),        
+              'status': fields.selection([
+                        ('active','Active'),
+                        ('inactive','InActive'),
+                        ('outofservice','Out of Service'),                        
+                        ], 'status', required=True,),
+              'ownership': fields.selection([
+                        ('owned','Owned'),
+                        ('leased','Leased'),
+                        ('rented','Rented'),                       
+                        ], 'Ownership', required=True),                               
+              'cmil':fields.float('Current Mileage',digits = (16,3)),
+              'bmil':fields.float('Base Mileage',digits=(16,3),help="The last recorded mileage"),
+              'bdate':fields.date('Recorded Date',help="Date on which the mileage is recorded"),
+              'pdate':fields.date('Purchase Date',help="Date of Purchase of vehicle"),
+              'pcost':fields.float('Purchase Value',digits=(16,2)),
+              
+              'ppartner':fields.many2one('res.partner','Purchased From'),              
+			  
+              'pinvoice':fields.char('Purchase Invoice',size=15),
+              'podometer':fields.integer('Odometer at Purchase'),
+              'startodometer':fields.integer('Start Odometer',required=True),                            
+              'deprecperc':fields.float('Depreciation in %',digits=(10,2),required=True),
+              'deprecperd':fields.selection([
+                                               ('monthly','Monthly'),
+                                               ('quarterly','Quarterly'),
+                                               ('halfyearly','Half Yearly'),
+                                               ('annual','Yearly')
+                                               ],'Depr. period',required=True),                
+              'primarymeter':fields.selection([
+                                                 ('odometer','Odometer'),
+                                                 ('hourmeter','Hour Meter'),
+                                                 ],'Primary Meter',required=True),
+              'fueltype':fields.selection([
+                                             ('hyrbrid','Hybrid'),
+                                             ('diesel','Diesel'),
+                                             ('gasoline','Gasoline'),
+                                             ('cng','C.N.G'),
+                                             ('lpg','L.P.G'),
+                                             ('electric','Electric')
+                                             ],'Fuel Used',required=True),             
+              'fueltankcap':fields.float('Fuel Tank Capacity'),
+              'warrexp':fields.date('Date',help="Expiry date for warranty of product"),
+              'warrexpmil':fields.integer('(or) Mileage',help="Expiry mileage for warranty of product"),         
+		      'veihcle_id': fields.one2many('car.rental.contract', 'veihcle_id' , 'Veihcle To Rent'),
+                                  
+              
+    }
+    _defaults = {
+        'type':lambda *a:'vehicle',
+        'status':lambda *a:'active',
+        'ownership':lambda *a:'owned',
+        'fueltype':lambda *a:'diesel',
+        'primarymeter':lambda *a:'odometer',
+        'deprecperd':lambda *a:'annual'
+        
+    }
+car_rental()
+       
+
+###################################
+#this Object for making a rental contract between the company and the customer
+
+class car_rental_contract(osv.osv):
+    _name='car.rental.contract'
+    #_inherits = {'product.product': 'product_id'}
+    #_inherit='product.product'
+    _description='Module for Car Rental Management'	
+    _columns={
+              'customer_id': fields.many2one('res.partner', 'Renter Name'),
+			  'veihcle_id':  fields.many2one('car.rental', 'Veihcle To Rent'),
+			  ############ AutoComplete this fields ################
+			  #type='many2one',relation='car.make',
+			  'car_make': fields.related('veihcle_id', 'car_make_id', readonly=True , type='char', string='Vehicle Make'),
+			  'car_brand': fields.related('veihcle_id', 'car_brand_id', readonly=True , type='char', string='Vehicle Brand'),
+			  'car_class': fields.related('veihcle_id', 'car_class_id', readonly=True , type='char', string='Vehicle Class'),
+			  'car_color': fields.related('veihcle_id', 'car_color_id', readonly=True , type='char', string='Vehicle Color'),
+			  
+			  ############
+              'cost':fields.float('Rent Cost',help='This fiels to determine the cost of rent per hour'),
+              'rent_start_date': fields.date('Rent Start Date', required=True),
+              'rent_end_date':fields.date('Rent End Date', required=True),
+              'rent_state':fields.selection([('cancelled','Cancelled'),('running','Running'),('finished','Finished')],'Rent State'),              
+              'notes':fields.text('Details'),
+                                                
+    }
+    _defaults = {
+        'rent_state' : lambda *a : 'running',        
+        'rent_start_date': lambda *a: time.strftime('%Y-%m-01'),
+        'rent_end_date': lambda *a: str(datetime.now() + relativedelta.relativedelta(months=+1, day=1, days=-1))[:10],
+        
+    }
+	
+
+car_rental_contract()
